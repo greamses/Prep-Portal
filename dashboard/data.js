7fcd3a49c36c2e87845c2cf804becd22a2daf7c2
Greamses <eemadanyel@gmail.com>
2026-03-31 10:23:13 +0100

    update

--- dashboard/data.js
+++ dashboard/data.js
@@ -26,7 +26,7 @@ const siteData = {
         { text: "Subjects", href: "#subjects" },
         { text: "Theory Drill", href: "#theory" },
         { text: "About", href: "#about" },
-        { text: "Authentication", href: "../auth.html" }
+        { text: "Authentication", href: "../utils/auth/auth.html" }
     ],
     ctaButton: { text: "Start Revising", href: "#exams" },
     
