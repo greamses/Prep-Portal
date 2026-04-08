802e6ccbd1afe6f130556437b00a15241dc750b2
Greamses <eemadanyel@gmail.com>
2026-04-08 05:37:14 +0100

    update

--- blogs/js/nav-data.js
+++ blogs/js/nav-data.js
@@ -3,16 +3,16 @@
 // nav-data.js
 export const navData = {
   logo: {
-    iconPath: "../../../logo/logo-light.svg",
+    iconPath: "../../../../logo/logo-light.svg",
     brandName: "PrepPortal",
     brandTop: "Prep",
     brandBottom: "Portal"
   },
   
   links: [
-    { name: "Dashboard", href: "../../../dashboard/dashboard.html", active: false },
-    { name: "Life and Health", href: "#", active: true },
-    { name: "Animals", href: "../../animal-blog/blog/blog-science.html", active: false, isCta: true },
+    { name: "Dashboard", href: "../../../../dashboard/dashboard.html", active: false },
+    { name: "Life and Health", href: "../../animal/blog/blog.html", active: true },
+    { name: "Animals", href: "../../animal/blog/blog.html", active: false, isCta: true },
     { name: "Earth Science", href: "#", active: false },
     { name: "Space Science", href: "#", active: false },
   ],
