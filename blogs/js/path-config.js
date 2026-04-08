9278d048cf9ba8a8aceea61413ce12978b755a42
Greamses <eemadanyel@gmail.com>
2026-04-08 12:47:47 +0100

    update

--- blogs/js/path-config.js
+++ blogs/js/path-config.js
@@ -8,38 +8,49 @@ const CONFIG = {
   
   // JS files  
   jsFiles: [
+
     '../../../../js/path-config.js',
-    '../../../js/nav.js',
-    '../../../../utils/loader/loader.js',
+    '../../../../js/nav.js',
+    '../../../../../utils/loader/loader.js',
     './blog.js'
   ],
   
   // Logo files
-  logoIcon: '../logo/logo-icon.svg',
-  logoLight: '../../../../logo/logo-light.svg',
-  logoLoading: '../logo/logo-loading.svg',
-  
-  // Load all CSS
+  logoIcon: '../../../../../logo/logo-light.svg',
+  logoLight: '../../../../../logo/logo-light.svg',
+  logoLoading: '../../../../../logo/logo-light.svg',
+    // Load all CSS
   loadAllCSS: function() {
-    this.cssFiles.forEach(file => {
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.href = file;
-      document.head.appendChild(link);
-    });
-  },
-  
-  // Load all JS
-  loadAllJS: function() {
-    this.jsFiles.forEach(file => {
-      const script = document.createElement('script');
-      script.src = file;
-      if (file.endsWith('.js') && !file.includes('loader')) {
-        script.type = 'module';
-      }
-      document.head.appendChild(script);
-    });
-  }
+      let loaded = 0;
+      this.cssFiles.forEach(file => {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = file;
+        link.onload = () => {
+          loaded++;
+          if (loaded === this.cssFiles.length) {
+            // All CSS loaded, show body
+            document.body.style.visibility = 'visible';
+          }
+        };
+        document.head.appendChild(link);
+      });
+    },
+    
+    // Load all JS
+    loadAllJS: function() {
+      this.jsFiles.forEach(file => {
+        const script = document.createElement('script');
+        script.src = file;
+        if (file.endsWith('.js') && !file.includes('loader')) {
+          script.type = 'module';
+        }
+        document.head.appendChild(script);
+      });
+    }
+
 };
 
 window.CONFIG = CONFIG;
+
+
