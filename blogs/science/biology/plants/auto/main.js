af63398cbad8828f33808f628074f971fcf035e1
greamses <eemadanyel@gmail.com>
2026-05-24 19:36:36 -0700

    latest

--- blogs/science/biology/plants/auto/main.js
+++ blogs/science/biology/plants/auto/main.js
@@ -2,5 +2,5 @@
 import * as plantlData from "/blogs/science/biology/plants/auto/data.js";
 import { initUI } from "/blogs/js/ui-controller.js";
 
-// Initialize the UI with Animals data
-initUI(animalData.SUBJECT_CONFIG, animalData);
+// Initialize the UI with Plants data
+initUI(plantData.SUBJECT_CONFIG, plantData);
